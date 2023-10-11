6d4e27e07bea23cc8799db59f6c6ca550b2408d2
Kia Shakiba <kiashakiba@protonmail.com>
2023-10-11 15:18:01 -0400

    Add int pointer example

--- main.cpp
+++ main.cpp
@@ -5,27 +5,15 @@
 #include <ece244/struct_test.hpp>
 
 int main() {
-	LinkedList list;
+	int num = 5;
+	int* ptr = &num;
 
-	list.push_front(3);
-	list.push_front(2);
-	list.push_front(1);
+	std::cout << "(before) num = " << num << std::endl;
 
-	std::cout << "is_empty: " << list.is_empty() << std::endl;
+	ptr += 8;
+	*ptr += 3;
 
-	list.print();
+	std::cout << "(after) num = " << num << std::endl;
 
 	return 0;
 }
-
-/*
-int main() {
-	Test test;
-
-	test.data = 6;
-	//test.set_data(5);
-
-	std::cout << "Data: " << test.get_data() << std::endl;
-
-	return 0;
-}*/
